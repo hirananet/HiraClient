8d2ba32ffef0c6638936dceceefc7457a62279a7
Alexander Eberle <alexander171294@gmail.com>
2021-04-12 14:50:50 -0300

    adding deployment script

--- web-client/build-docker.sh
+++
@@ -1,4 +0,0 @@
-ng build --prod
-docker build -t alexander171294/hira-client .
-docker tag alexander171294/hira-client alexander171294/hira-client:core-v2
-docker push alexander171294/hira-client:core-v2
